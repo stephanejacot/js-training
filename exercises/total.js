611d50b510114cc47a185a80cbe6d7348eeae600
Stephane Jacot <stephanejacot@icloud.com>
2019-03-14 12:11:55 +0100

    total adding

--- exercises/total.js
+++ exercises/total.js
@@ -5,7 +5,10 @@
  * and return the total
  *
  */
-
+let Array = [1, 1, 1, 10, 10, 10, 24, -10, 10, 0, 0, 100];
+function total (array) {
+    return array.reduce( (accumulator, currentValue) => accumulator + currentValue);
+}  
 
 //* Begin of tests
 const assert = require('assert')
