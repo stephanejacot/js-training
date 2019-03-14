ebc624c4e220db6470f1ce4ac3173f4a4e7d6300
Stephane Jacot <stephanejacot@icloud.com>
2019-03-14 11:31:54 +0100

    same-sign adding

--- exercises/same-sign.js
+++ exercises/same-sign.js
@@ -8,7 +8,13 @@
  */
 
 // Your code :
-
+function sameSign (number1,  number2){
+    if (number1 >0 && number2 >0 || number1 <0 && number2 <0 || number1 ===0 && number2 ===0){
+        return true;
+    } else {
+        return false;
+    }
+}
 //* Begin of tests
 const assert = require('assert')
 
