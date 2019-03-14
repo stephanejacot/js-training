146961dded17c5b82dadc93d5ca0bd7d8e0660da
Stephane Jacot <stephanejacot@icloud.com>
2019-03-14 16:47:16 +0100

    get length  adding

--- exercises/get-length.js
+++ exercises/get-length.js
@@ -7,12 +7,13 @@
  * @notions Data-Structures, Get
  */
 
-// Your code :
+let getLength = ['pouet', 'salut']
+    return (getLength.length);
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.strictEqual(getLength([ 2, 42 ]), 2)
+assert.strictEqual(getLength([ 2,42 ]), 2)
 assert.strictEqual(getLength([ 'pouet', 4, true ]), 3)
 assert.strictEqual(getLength(Array(100)), 100)
 assert.strictEqual(getLength([ getLength ]), 1)
--- exercises/multiply.js
+++ exercises/multiply.js
@@ -9,31 +9,15 @@
  */
 
 // Your code :
-let total = 0;
+let sum = 0;
 let i = 0;
 function multiply (number1,  number2){
     while (i <= number2) {
-        total += number1;
+        sum += number1;
         i++;
     }
 }
-return multiply
-//     if (number1 ===0 || number2 ===0){
-//         return 0;
-//     }  else if (number1 >0){
-//         while (i <= number2) {
-//             total = total + number1;
-//             i++;
-//             return total
-//         }
-//     }   else if (number1 >0){
-//         while (i <= number2) {
-//             total = total + number1;
-//             i++;
-//             return total
-//             }
-//     }
-// }
+return sum
 
 //* Begin of tests
 const assert = require('assert')
