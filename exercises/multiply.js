e318fc15926a580a54f57d46d06c31a3a1613426
Stephane Jacot <stephanejacot@icloud.com>
2019-03-14 13:45:25 +0100

    multiply adding

--- exercises/multiply.js
+++ exercises/multiply.js
@@ -9,6 +9,31 @@
  */
 
 // Your code :
+let total = 0;
+let i = 0;
+function multiply (number1,  number2){
+    while (i <= number2) {
+        total += number1;
+        i++;
+    }
+}
+return multiply
+//     if (number1 ===0 || number2 ===0){
+//         return 0;
+//     }  else if (number1 >0){
+//         while (i <= number2) {
+//             total = total + number1;
+//             i++;
+//             return total
+//         }
+//     }   else if (number1 >0){
+//         while (i <= number2) {
+//             total = total + number1;
+//             i++;
+//             return total
+//             }
+//     }
+// }
 
 //* Begin of tests
 const assert = require('assert')
